46f934c5ee7d6b9fcff4909b57c98c73e77b863e
Noel-96 <noelorion69@gmail.com>
2021-07-03 18:35:01 +0100

    init repo

---
+++ Backdrop-Coding-Challenge/Backdrop_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Backdrop_Coding_ChallengeApp.swift
+//  Backdrop-Coding-Challenge
+//
+//  Created by Noel Obaseki on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Backdrop_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
